c8abf9282f2ba4dd412820c86a6d610866845825
LBytes-32 <LukeBates20@gmail.com>
2023-07-11 17:28:27 -0400

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom/client'
 
 import App3 from "./ch3/App"
 import App4 from './ch4/App'
-import ChK from "./chK/App"
+//import ChK from "./chK/App"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App3/>
+    <App4/>
   </React.StrictMode>,
 )
